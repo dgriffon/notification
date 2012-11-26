1bff032262c090c772c950f2fd4c4c49d1f40ef1
David Griffon <dgriffon@jahia.com>
2012-11-26 11:16:04 +0100

    add mail

--- src/main/java/org/jahia/modules/notification/rules/SendMailToSubscribersRuleService.java
+++ src/main/java/org/jahia/modules/notification/rules/SendMailToSubscribersRuleService.java
@@ -1,17 +1,27 @@
 package org.jahia.modules.notification.rules;
 
+import org.apache.commons.lang.StringUtils;
 import org.drools.spi.KnowledgeHelper;
+import org.jahia.services.SpringContextSingleton;
+import org.jahia.services.content.JCRContentUtils;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRValueWrapper;
 import org.jahia.services.content.decorator.JCRUserNode;
 import org.jahia.services.content.rules.AddedNodeFact;
 import org.jahia.services.mail.MailService;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.URLResolver;
+import org.jahia.services.render.URLResolverFactory;
+import org.jahia.services.seo.urlrewrite.UrlRewriteService;
 import org.jahia.settings.SettingsBean;
 import org.jahia.utils.LanguageCodeConverters;
+import org.tuckey.web.filters.urlrewrite.RewrittenUrl;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
 import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequestWrapper;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -28,6 +38,9 @@ public class SendMailToSubscribersRuleService {
     private MailService mailService;
     private String mailTemplatePath;
     private String from;
+    private URLResolverFactory urlResolverFactory;
+    private String URLGenericPrefix;
+    private UrlRewriteService urlRewriteService;
 
     public void sendMailToSubscribers(final AddedNodeFact nodeFact, KnowledgeHelper drools) throws RepositoryException,ScriptException {
         JCRNodeWrapper n = nodeFact.getNode();
@@ -35,10 +48,18 @@ public class SendMailToSubscribersRuleService {
         from = "".equals(from)? SettingsBean.getInstance().getMail_from():from;
         final Map<String,Object> bindings = new HashMap<String,Object>();
         bindings.put("node",n);
+        // Resolve Node url
+
+        JCRNodeWrapper displayableNode = JCRContentUtils.findDisplayableNode(n,new RenderContext(null,null,n.getUser()));
+
+        String url = URLGenericPrefix.replace("##serverName##",n.getResolveSite().getServerName()) + "/##lang##" + displayableNode.getPath() + ".html";
+
         if (p.hasProperty("subscribers")) {
             for (Value v:p.getProperty("subscribers").getValues()) {
                 JCRUserNode u = (JCRUserNode) n.getSession().getNodeByUUID(v.getString());
                 bindings.put("currentUser",u);
+                bindings.put("displayNode",displayableNode);
+                bindings.put("urlDisplayNode",url);
                 String to = u.getProperty("j:email").getString();
                 Locale l = LanguageCodeConverters.languageCodeToLocale(u.getProperty("preferredLanguage").getString());
                 mailService.sendMessageWithTemplate(mailTemplatePath,bindings,to,from,null,null,l,"notification");
@@ -56,5 +77,17 @@ public class SendMailToSubscribersRuleService {
 
     public void setFrom(String from) {
         this.from = from;
+    }
+
+    public void setUrlResolverFactory(URLResolverFactory urlResolverFactory) {
+        this.urlResolverFactory = urlResolverFactory;
+    }
+
+    public void setUrlRewriteService(UrlRewriteService urlRewriteService) {
+        this.urlRewriteService = urlRewriteService;
+    }
+
+    public void setURLGenericPrefix(String URLGenericPrefix) {
+        this.URLGenericPrefix = URLGenericPrefix;
     }
 }
